f8a9bddc5cafc78d8a87ea9cb1585d8e5c6cdd7d
AmMejay <nicolasrb08@gmail.com>
2026-06-30 15:08:43 -0300

    Melhorias e Explicação

--- routes/routes.php
+++ routes/routes.php
@@ -7,28 +7,38 @@ require_once __DIR__ . "/../app/repository/PessoaRepository.php";
 require_once __DIR__ . "/../app/service/PessoaService.php";
 require_once __DIR__ . "/../app/controllers/PessoaController.php";
 
-$pessoaRepository = new PessoaRepository($pdo);
-$pessoaService = new PessoaService($pessoaRepository);
-$pessoaController = new PessoaController($pessoaService);
+//idiciação
+$pessoaRepository = new PessoaRepository($pdo); //repositório pede o banco de dados
+$pessoaService = new PessoaService($pessoaRepository); //pessoaService pega as infos que o repositório pegou do PDO.
+$pessoaController = new PessoaController($pessoaService); //pessoaController pega tudo pronto para utilizar do PDO.
 
-return [
+return [ //entra aqui e retorna as rotas sempre.
 
     [
-        'POST',
+        'POST', //organiza o método para API
         '/pessoas/criar',
-        function (array $payload) use ($pessoaController) {
+        function (array $payload) use ($pessoaController) { // $payload é um Array vázio de index.php e usa as informações do banco de dados PDO
 
-            if (!isset($payload['nome'])) {
+            if (!isset($payload['nome'])) { // valida se tem algo
 
-                http_response_code(400);
+                http_response_code(400); 
 
                 return json_encode([
                     'success' => false,
                     'message' => 'Campo nome é obrigatório.'
                 ]);
             }
+            if (empty($payload['nome'])) { // valida se não é vázio
+                
+                http_response_code(400); 
 
-            $pessoa = new Pessoa($payload['nome']);
+                return json_encode([
+                    'success' => false,
+                    'message' => 'Campo nome está vázio.'
+                ]);
+            }                                               
+
+            $pessoa = new Pessoa($payload['nome']); // tudo certo então cria a pessoa se foi essa a rota escolhida.
 
             return $pessoaController->create($pessoa);
         }
@@ -48,8 +58,16 @@ return [
                     'message' => 'Campo id é obrigatório.'
                 ]);
             }
+            if (empty($payload['id'])) {
+                http_response_code(400);
 
-            return $pessoaController->delete($payload['id']);
+                return json_encode([
+                    'success' => false,
+                    'message' => 'Campo id está vázio.'
+                ]);
+            }
+
+            return $pessoaController->delete($payload['id']); //deleta os dados baseado no ID recebido
         }
     ]
 
