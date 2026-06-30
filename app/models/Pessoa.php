0081d4afce973ac9d8210dfc077574d34ac05e94
AmMejay <nicolasrb08@gmail.com>
2026-06-30 15:37:40 -0300

    Explicando código

--- app/models/Pessoa.php
+++ app/models/Pessoa.php
@@ -2,31 +2,35 @@
 
 class Pessoa
 {
-    private int | null $id;
-    private string $nome;
+    # public = Fora do código ou programa para utilizar.
+    # private = Apenas dentro da classe onde foi criado.
+    
+    private int | null $id; // tipagem dos dados. perfeito.
+    private string $nome; // tipagem dos dados. perfeito.
 
-    public function __construct(string $nome)
+    public function __construct(string $nome) // contruct serve para criar um new object e nesse caso fizemos um objeto com uma var tipo string que recebe algum valor.
     {
-        $this->nome = $nome;
+        $this->nome = $nome; // this é para dizer que queremos acessar algo no objeto nome e por o valor da var, tipo $nome = "Nícolas Gostoso";  $this->nome = $nome.
     }
 
-    public function getId(): int
+    // ID pode ser null então melhorei colocando ? antes do int
+    public function getId(): ?int
     {
-        return $this->id;
+        return $this->id; // pega o ID recebido pelo sistema.
     }
 
     public function getNome(): string
     {
-        return $this->nome;
+        return $this->nome; // pega o nome recebido pelo sistema.
     }
 
     public function setId(int $id): void
     {
-        $this->id = $id;
+        $this->id = $id; // seta o valor ID no banco de dados.
     }
 
-    public function setNome(int $nome): void
+    public function setNome(string $nome): void
     {
-        $this->nome = $nome;
+        $this->nome = $nome; // seta o nome no banco de dados.
     }
 }
